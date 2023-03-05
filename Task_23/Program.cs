5cc4bd8bbc37bd4640404d09dfd8d9495895b4a0
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-05 22:03:29 +0300

    Задание 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int num = InputNumber("Введите число ");
+Console.WriteLine($"Таблица кубов чисел от 1 до {num}: ");
+Cubes(num);
+static int InputNumber(string message)
+{
+    try
+    {
+        Console.Write(message);
+        return (int.Parse(Console.ReadLine() ?? ""));
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
+
+static void Cubes(int num)
+{
+    for (int i = 1; i <= num; i++)
+{
+   Console.Write($"{i * i * i}, ");
+}
+}
